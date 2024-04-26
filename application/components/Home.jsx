e9ab7dbe8a737b18e31ff0f2a4c0169069446679
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-04-26 17:37:21 +0530

    completed home ui

--- application/components/Home.jsx
+++ application/components/Home.jsx
@@ -1,48 +1,35 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { useFonts } from 'expo-font';
-import { Link } from 'expo-router';
-import GradientBackground from "./GradientBackground";
-
-
+import { Link } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const options = [
-  {
-    title: "Expressify",
-    imagePath: require("../assets/icons/expressify.png"),
-    path:"/expressify"
-  },
-  {
-    title: "Signify",
-    imagePath: require("../assets/icons/signify.png"),
-    path:"/signify"
-  },
-  {
-    title: "Resources",
-    imagePath: require("../assets/icons/resources.png"),
-    path:"/resources"
-  },
-];
+    {
+      title: "Expressify",
+      imagePath: require("../assets/icons/expressify.png"),
+      path:"/expressify"
+    },
+    {
+      title: "Signify",
+      imagePath: require("../assets/icons/signify.png"),
+      path:"/signin"
+    },
+    {
+      title: "Resources",
+      imagePath: require("../assets/icons/resources.png"),
+      path:"/resources"
+    },
+  ];
 
 export default function Home() {
-  const [fontsLoaded] = useFonts({
-    'Planquin-Black': require('../assets/fonts/Palanquin_Dark/PalanquinDark-Regular.ttf'),
-    'Planquin-Bold': require('../assets/fonts/Palanquin_Dark/PalanquinDark-Bold.ttf'),
-  });
-  if (!fontsLoaded) {
-    return null;
-  }
   return (
-    <GradientBackground styles={styles.container}>
-      <View style={styles.innerContainer}>
-        <View style= {styles.aboutContainer}>
-          <Text style={styles.heading2}>Nexus</Text>
-          <Image source={require("../assets/icons/gear.png")} style={styles.gear} />
+    <View style={styles.container}>
+      <View style={styles.circle}></View>
+      <View style={styles.circle2}></View>
+      <View style={styles.welcomeContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.heading}>Expressify</Text>
+            <Text style={{width: 300,color:"white"
+            }}>Concentrate on the abilities your disability doesn't hinder and don't dwell on the things it interferes with</Text>
         </View>
-        <View style={styles.welcomeContainer}>
-          <Text style={styles.heading}>Welcome</Text>
           <View style={styles.group}>
             {options.map((option) => (
               <View style={styles.optionsContainer} key={option.title}>
@@ -58,35 +45,48 @@ export default function Home() {
             ))}
           </View>
         </View>
-      </View>
-      <Image
-        source={require("../assets/robot.png")}
-        style={styles.robot}
-        resizeMode="contain"
-      />
-    </GradientBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  innerContainer: {
-    flex: 1,
-    alignItems: "center",
-    // justifyContent: "right",
-    height: "100%",
-    width: "100%",
-    top:100,
-  },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     height: "100%",
     width: "100%",
-    fontFamily: "Planquin-Black",
+    backgroundColor: "#D7E5FF",
+  },
+  circle: {
+    width: 398,
+    height: 398,
+    borderRadius: 500 / 2,
+    backgroundColor: "#9BBEFD",
+    position: "absolute",
+    top: -292,
+    left: -163,
+    zIndex: 5,
+  },
+  circle2: {
+    width: 700,
+    height: 700,
+    borderRadius: 1000,
+    backgroundColor: "#367CFED9",
+    position: "absolute",
+    top: -409,
+    left: -200,
+    zIndex: 3,
+  },
+  image: {
+    height: 70,
+    width: 70,
+    borderRadius: 50, 
+    overflow: "hidden", 
+    zIndex: 10,
   },
   rectangle: {
-    backgroundColor: "#343341",
+    backgroundColor: "white",
     width: 250,
     height: "contain",
     borderRadius: 10,
@@ -96,28 +96,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 0,
   },
-  text: {
-    fontSize: 30,
-    color: "#fff",
-    fontFamily: "Planquin-Black",
-  },
-  group: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    gap: 10,
-  },
   optionsContainer: {
     display: "flex",
     flexDirection: "row",
+    alignItems: "center",
     gap: -20,
-  },
-  image: {
-    height: 70, // specify your desired height
-    width: 70, // specify your desired width
-    borderRadius: 50, // to make it round, set borderRadius to half of the height (or width)
-    overflow: "hidden", // necessary to make borderRadius work
-    zIndex: 10,
   },
   optionTitleContainer: {
     display: "flex",
@@ -125,43 +108,45 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  robot: {
-    position: "absolute",
-    bottom: -10,
-    right: 20,
-    width: 140,
+  group: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    gap: 10,
+    marginTop:-250
   },
   heading:{
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: "700",
-    color: "#000",
+    color: "white",
   },
   heading2:{
     fontSize: 30,
     fontFamily: "Planquin-Black",
     color: "#000",
   },
-  aboutContainer:{
-    display: "flex",
-    flexDirection: "row",
-    alignContent: "flex-start",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    width: "100%",
-    marginRight: 50,
-    gap: 10,
-  },
-  gear: {
-    height: 40,
-    width: 40, 
-    borderRadius: 50, 
-    overflow: "hidden",
-    zIndex: 10,
+  text: {
+    fontSize: 30,
+    color: "#000",
+    fontFamily: "Planquin-Black",
   },
   welcomeContainer:{
     marginTop: 20,
     display: "flex",
     flexDirection: "column",
-    gap: 20,
+    justifyContent: "space-around",
+    alignContent: "center",
+    height: "100%",
+    zIndex:100,
+  },
+  titleContainer:{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    width: "100%",
+    overflow: "hidden",
+    marginTop:-100
   }
 });
