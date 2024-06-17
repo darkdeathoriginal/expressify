8e4997f8981ff7bfd87f0e143c8638141abef49c
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-06-17 19:12:31 +0530

    chore: Update app.json and eas.json configuration files

--- application/firebaseconfig.js
+++ application/firebaseconfig.js
@@ -3,12 +3,12 @@ import { initializeAuth, getReactNativePersistence, ge } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAYbKa_VDvfPxXF9E5uUUIMF6U42l8b_4U",
-  authDomain: "expressify-dec35.firebaseapp.com",
-  projectId: "expressify-dec35",
-  storageBucket: "expressify-dec35.appspot.com",
-  messagingSenderId: "1062782089457",
-  appId: "1:1062782089457:web:df9f7a035e71d8769d1150",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
